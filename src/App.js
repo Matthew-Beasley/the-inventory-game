7e5cc7293e79f8f9bf832a44b558b5a23ce2119a
conbec <56614504+conbec@users.noreply.github.com>
2020-01-17 14:18:02 -0800

    Working except delete and modularzation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './data';
 import './App.css';
 
+
 function App() {
+  const { products } = data;
+  const [items, setProducts] = useState(products)
+
+  const add = (id) => { 
+    items.forEach(item => {
+      if (item.id === id && item.numberInStock < 10) {
+        item.numberInStock++;
+      }
+    });
+    setProducts([...items])
+  }
+  
+  const remove = (id) => { 
+    items.forEach(item => {
+      if (item.id === id && item.numberInStock > 0) {
+        item.numberInStock--;
+      }
+    });
+    setProducts([...items])
+  } 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+      {items.map(item => {
+        return (
+          <li key={item.id}>{item.name} {item.numberInStock}
+            <button onClick={(e) => add(item.id)} className="add">Add to Inventory</button>
+            <button onClick={(e) => remove(item.id)}className="subtract">Remove from Inventory</button>
+          </li>
+        )
+      })}
+      </ul>
     </div>
   );
 }
